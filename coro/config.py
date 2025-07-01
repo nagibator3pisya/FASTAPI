1c83ff5c62f83ef5779ffaf78eab7afa2ba1db79
user <morevstas02@mail.ru>
2025-07-01 12:24:22 +0300

    add echo = поменять на fals

--- coro/config.py
+++ coro/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Setting(BaseSettings):
     db_url: str = 'mysql+aiomysql://root:root@localhost:3306/fast_api'
-
+    db_echo : bool = True # ПОСТАВИТЬ на false что бы не замедлять работу
 
 setting = Setting()
 print(setting.db_url)
