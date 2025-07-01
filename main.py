f026335ef841d2f03ad890682928a03b6119b547
user <morevstas02@mail.ru>
2025-07-01 16:47:20 +0300

    роутер новый

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
 from coro.models import Base,db_help
+from coro.product.views import product_router
 from items_v import router_items
 from users.views import router_user
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_help.bd_helps.engine.begin() as conn:
+    async with db_help.bd_helps_exempl.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
 
@@ -18,6 +19,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(router_items)
 app.include_router(router_user)
+app.include_router(product_router)
 '''
 get - получить
 post - добавить
