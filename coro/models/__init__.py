f73ab6890ee1ae79535bc3764d4b907e7dab1559
user <morevstas02@mail.ru>
2025-07-01 16:57:02 +0300

    роутер новый

--- coro/models/__init__.py
+++ coro/models/__init__.py
@@ -1,11 +1,11 @@
 __all__ = (
     'Base',
     'Product',
-    'bd_helps',
+    'bd_helps_exempl',
     'DatabaseHelper',
 )
 
 
 from .base import Base
-from .db_help import DatabaseHelper,bd_helps
+from .db_help import DatabaseHelper,bd_helps_exempl
 from .product import Product
--- coro/product/views.py
+++ coro/product/views.py
@@ -1,33 +1,37 @@
 from fastapi import APIRouter,HTTPException,status
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from . import crud
 from .schemas import Product,ProductCreate
-product_router = APIRouter(tags=['Products'])
+from coro.models import db_help, bd_helps_exempl
+
+product_router = APIRouter(tags=['Products'],prefix='/products')
 """
 response_model - аннотация, модель ответа
 """
 
+
 @product_router.get('/',response_model=list[Product])
-async def get_products(session):
+async def get_products(session: AsyncSession = Depends(bd_helps_exempl.get_scoped_session)):
     # обычное чтение
     return await crud.get_products(session=session)
 
+
+
 @product_router.post('/',response_model=Product)
-async def create_product(session, product_in:ProductCreate):
+async def create_product(
+        product_in:ProductCreate,
+        session: AsyncSession = Depends(bd_helps_exempl.get_scoped_session)):
     # создание
     return await crud.create_product(session=session,product_in=product_in)
 
 
 
 
-@product_router.get('/{product_id}',response_model=Product)
-async def get_product(product_id:int,session):
+@product_router.get('/{product_id}/',response_model=Product)
+async def get_product(product_id:int,session: AsyncSession = Depends(bd_helps_exempl.get_scoped_session)):
     # чтение по id
     product = await crud.get_product(session=session,product_id=product_id)
     # если он пришел то возвращаем его
-    if product not in None:
-        return product
-    # если нет то 404
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f'Продукт {product_id} не найден'
-    )
+    return product
