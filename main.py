9fe945117f18be6ab624151d229f1ea1e130cd5c
user <morevstas02@mail.ru>
2025-07-01 12:23:24 +0300

    ссобытия жизненного цикла (запуск и завершение работы)

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 import uvicorn
-
+from coro.models import Base,db_help
 from items_v import router_items
 from users.views import router_user
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_help.bd_helps.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(router_items)
 app.include_router(router_user)
 '''
