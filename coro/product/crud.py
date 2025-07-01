28e43bc2362c1a07362b7be27396594a68a52793
user <morevstas02@mail.ru>
2025-07-01 14:42:30 +0300

    Запросы к бд по продуктам

---
+++ coro/product/crud.py
@@ -0,0 +1,63 @@
+'''
+Создание
+Чтение
+Обновить
+Удалить
+'''
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from  sqlalchemy.ext.asyncio import AsyncSession
+
+from coro.models import Product
+from coro.product.schemas import ProductCreate
+
+
+async  def get_products(session:AsyncSession)-> list[Product]:
+    """
+    Чтение
+    :param session:
+    :return:
+    """
+    srmt = select(Product).order_by(Product.id)
+    result:Result = await session.execute(srmt)
+    product = result.scalar().all()
+    return list(product)
+
+
+
+
+async def get_product(session:AsyncSession,product_id:int)-> Product | None:
+    """
+    Чтение по id
+    :param session:
+    :param product_id:
+    :return:
+    """
+    return await session.get(Product,product_id)
+
+
+
+async def create_product(session:AsyncSession,product_in:ProductCreate) -> Product:
+    product = Product(**product_in.model_dump())
+    session.add(product)
+    await session.commit()
+    return product
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
