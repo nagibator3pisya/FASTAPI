46c75e148304a31471c5d2b363ef436ee6e53143
user <morevstas02@mail.ru>
2025-07-01 14:42:50 +0300

    модель для продуктов

--- coro/models/product.py
+++ coro/models/product.py
@@ -1,10 +1,11 @@
-from sqlalchemy import String
+from sqlalchemy import String, Integer
 from sqlalchemy.orm import Mapped
 from sqlalchemy.testing.schema import mapped_column
 
 from .base import Base
 
-class User(Base):
-    __tablename__ = 'users'
-
-    name: Mapped[str] = mapped_column(String(20))
+class Product(Base):
+    __tablename__ = 'product'
+    name: Mapped[str] = mapped_column(String(50))
+    description: Mapped[str] = mapped_column(String(50))
+    price: Mapped[int] = mapped_column(Integer())
