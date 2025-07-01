eef2f24eed0cd4a0fbc5199b4af70655b1e85a97
user <morevstas02@mail.ru>
2025-07-01 12:08:14 +0300

    импорты _init__

---
+++ coro/models/__init__.py
@@ -0,0 +1,11 @@
+__all__ = (
+    'Base',
+    'Product',
+    'bd_helps',
+    'DatabaseHelper',
+)
+
+
+from .base import Base
+from .db_help import DatabaseHelper,bd_helps
+from .product import Product
